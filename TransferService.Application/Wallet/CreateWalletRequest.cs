f01c045bd20e302dd71eb5a7b2df3951312c8106
Carlos Aguilar <carlosaguilardiaz24@gmail.com>
2026-07-11 13:48:47 -0600

    feat(application): agregar wallet request y wallet service

---
+++ TransferService.Application/Wallet/CreateWalletRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransferService.Application.Wallet
+{
+    public class CreateWalletRequest
+    {
+        public string DocumentId {  get; set; }
+        public string Name { get; set; }
+        public decimal InitialBalance { get; set; }
+
+    }
+}
