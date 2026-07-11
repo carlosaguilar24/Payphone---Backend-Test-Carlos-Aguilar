a5c4ce2095f8d259500e994b8c74243a09b2099d
Carlos Aguilar <carlosaguilardiaz24@gmail.com>
2026-07-11 14:53:48 -0600

    feat(application) se creo DI para mapear services

--- TransferService.Application/Wallets/WalletService.cs
+++ TransferService.Application/Wallets/WalletService.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TransferService.Application.Wallet
+namespace TransferService.Application.Wallets
 {
     public class WalletService
     {
