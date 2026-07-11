1b76ea4839eee0f1d190e46ba11827fcfef45d4e
Carlos Aguilar <carlosaguilardiaz24@gmail.com>
2026-07-11 13:20:52 -0600

    feat(application): definir transfer request/reponse y creación de transferService para lógica de transferencia

---
+++ TransferService.Application/Transfer/TransferRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransferService.Application.Transfer
+{
+    public class TransferRequest
+    {
+        public int FromWalletId { get; set; }
+        public int ToWalletId { get; set; }
+        public decimal Amount { get; set; }
+        public Guid TransferId { get; set; }
+    }
+}
---
+++ TransferService.Application/Transfer/TransferResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransferService.Application.Transfer
+{
+    public class TransferResponse
+    {
+        public bool Success { get; set; }   
+        public string? Message { get; set; }
+
+        private TransferResponse(bool  success, string? message)
+        {
+            Success = success;
+            Message = message;
+        }
+
+        public static TransferResponse Ok() => new(true, null);
+        public static TransferResponse Error(string message) => new(false, message);    
+    }
+}
