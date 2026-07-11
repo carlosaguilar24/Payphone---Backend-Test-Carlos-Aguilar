7a6e81dd904a2e19ba2cff2e68d3d5ca48181984
Carlos Aguilar <carlosaguilardiaz24@gmail.com>
2026-07-11 12:57:52 -0600

    feat(application): definir contratos de repositorios de Wallet y Movement

--- TransferService.Application/Interfaces/IMovementRepository.cs
+++ TransferService.Application/Interfaces/IMovementRepository.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TransferService.Domain.Entities;
 
 namespace TransferService.Application.Interfaces
 {
     public interface IMovementRepository
     {
+        Task AddMovementAsync(Movement movement);
+        Task<IReadOnlyList<Movement>> GetMovementByWalletIdAsync(int walletId);
+        Task<IReadOnlyList<Movement>> GetMovementByIdTransactionAsync(Guid transactionId);
+
     }
 }
--- TransferService.Application/Interfaces/IWalletRepository.cs
+++ TransferService.Application/Interfaces/IWalletRepository.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TransferService.Domain.Entities;
 
 namespace TransferService.Application.Interfaces
 {
     public interface IWalletRepository
     {
+        Task<Wallet?> GetWalletByIdAsync(int id);
+        Task CreateWalletAsync(Wallet wallet);
+        Task UpdateWalletAsync(Wallet wallet);
     }
 }
